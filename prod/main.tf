a82d219db78236d83954d937f47ae5347dfffc5a
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-04-15 00:03:52 -0400

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 module "vpc" {
   source   = "../modules/vpc"
   vpc_cidr = "10.1.0.0/16"
-  vpc_name = "Staging-VPC"
+  vpc_name = "Prod-VPC"
   # Four public subnets and two private subnets
   public_subnet_cidrs  = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24", "10.1.4.0/24"]
   private_subnet_cidrs = ["10.1.5.0/24", "10.1.6.0/24"]
