98f9d657106a339b22f44e5b6653674d97eeef86
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-04-15 00:15:05 -0400

    Update main.tf

--- staging/main.tf
+++ staging/main.tf
@@ -105,7 +105,7 @@ resource "aws_security_group" "bastion_sg" {
     description = "Allow HTTP access from your IP"
     from_port   = 80
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Replace YOUR_PUBLIC_IP with your actual public IP
   }
 
